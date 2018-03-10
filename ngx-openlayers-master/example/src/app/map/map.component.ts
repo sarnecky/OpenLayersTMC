3b7a3b4f8aef4088a77d415d51b9f34502c874a3
Sebastian Sarnecki <ssarnecki34@gmail.com>
2018-03-10 18:37:30 +0100

    calculating distance

--- ngx-openlayers-master/example/src/app/map/map.component.ts
+++ ngx-openlayers-master/example/src/app/map/map.component.ts
@@ -1,4 +1,7 @@
 import {Component} from '@angular/core';
+import ol from 'ol';
+import Sphere from 'ol/sphere';
+import Geometry from 'ol/geom/geometry';
 
 @Component({
     selector: 'tmc-map',
@@ -7,4 +10,23 @@ import {Component} from '@angular/core';
 })
 export class MapComponent{
     public zoom = 7;
+
+    catchEvent(event){
+        var polygon = event.feature.O.geometry;
+        console.log(this.calculateDistance(polygon));
+    }
+
+    calculateDistance(polygon: ol.geom.Geometry): number {
+        console.log("calculate")
+        console.log(polygon)
+        var area = Sphere.getArea(polygon);
+        var output;
+        if(area > 10000){
+            output = (Math.round(area/1000000 * 100) / 100) + ' km<sup>2</sup>'
+        }
+        else{
+            output = (Math.round(area*100)/100)+ 'm<sup>2</sup>';
+        }
+        return output;
+    }
 } 
