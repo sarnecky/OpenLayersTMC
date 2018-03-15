d2a131bee91c9eb34c1513969b5ccbfd612fbbbd
Sebastian Sarnecki <ssarnecki34@gmail.com>
2018-03-15 08:23:59 +0100

    to fix

--- ngx-openlayers-master/example/src/app/map/map.component.ts
+++ ngx-openlayers-master/example/src/app/map/map.component.ts
@@ -1,7 +1,12 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, Input } from '@angular/core';
 import ol from 'ol';
 import Sphere from 'ol/sphere';
 import Geometry from 'ol/geom/geometry';
+import Style from 'ol/style/style';
+import Stroke from 'ol/style/stroke';
+import Circle from 'ol/style/circle';
+import Fill from 'ol/style/fill';
+import MultiPoint from 'ol/geom/multipoint';
 import { Polygon } from "../../CommonModels/polygon";
 
 @Component({
@@ -11,13 +16,43 @@ import { Polygon } from "../../CommonModels/polygon";
 })
 export class MapComponent{
     public zoom = 7;
-    
+    public polygonName: string;
+    public a: Sphere;
+    public polygonStyle: Array<Style>;
     @Output() onPolygonCreated: EventEmitter<Polygon>;
     
     constructor(){
+        this.polygonStyle = new Array<Style>();
+        var firstStyle = new Style({
+            stroke: new Stroke({
+              color: 'blue',
+              width: 3
+            }),
+            fill: new Fill({
+              color: 'rgba(0, 0, 255, 0.1)'
+            })
+          });
+        var secondStyle = new Style({
+            image: new Circle({
+              radius: 5,
+              fill: new Fill({
+                color: 'orange'
+              })
+            }),
+            geometry: function(feature) {
+              // return the coordinates of the first ring of the polygon
+              var coordinates = feature.getGeometry().getCoordinates()[0];
+              return new MultiPoint(coordinates);
+            }
+          })
+        this.polygonName = "Polygon Seby hehe";
         this.onPolygonCreated = new EventEmitter<Polygon>();
+        this.polygonStyle.push(firstStyle, secondStyle);
+        
     }
-
+    getPolygonSyle() : ol.style.Style{
+        return this.polygonStyle;
+    }
     catchDrawEndEvent(event){
         var polygon = event.feature.O.geometry;
         var distance = this.calculateDistance(polygon);
@@ -26,7 +61,7 @@ export class MapComponent{
         this.onPolygonCreated.emit(polygonForEvent)
     }
 
-    calculateDistance(polygon: ol.geom.Geometry): number {
+    calculateDistance(polygon: Geometry): number {
         console.log("calculate")
         console.log(polygon)
         var area = Sphere.getArea(polygon);
